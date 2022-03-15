107ad689c1bdad700e65be707f94dcc7b4b6a1ad
symond <39340668+symond2635@users.noreply.github.com>
2022-03-16 03:01:04 +0900

    Update export.py

--- export.py
+++ export.py
@@ -1,18 +1,22 @@
 import os
 import subprocess
 
-#自分の相対パス取得
-path = os.path.dirname(__file__)
+def main():
+    #自分の相対パス取得
+    path = os.path.dirname(__file__)
 
-#自分のファイルパスにcd
-os.chdir(path)
+    #自分のファイルパスにcd
+    os.chdir(path)
 
-#確認dir
-subprocess.call('dir', shell=True)
+    #確認dir
+    subprocess.call('dir', shell=True)
 
-#エクスポート
-print("\nエクスポートを実行します。\n")
-subprocess.call('cscript vbac.wsf decombine', shell=True)
+    #エクスポート
+    print("\nエクスポートを実行します。\n")
+    subprocess.call('cscript vbac.wsf decombine', shell=True)
 
-#pause
-os.system("pause")
+    #pause
+    os.system("pause")
+
+if __name__ == "__main__"():
+    main()
